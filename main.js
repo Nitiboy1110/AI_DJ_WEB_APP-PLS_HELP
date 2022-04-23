32d06c0a149c5ee9a0765a4f5b4b06e205c3594c
NITYAM <83535754+Nitiboy1110@users.noreply.github.com>
2022-04-23 17:22:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+
+
+
+leftWrist_X=0;
+rightWrist_X=0;
+leftWrist_Y=0;
+rightWrist_Y=0;
+
+function preload()
+{
+     if(leftWrist_X,leftWrist_Y ){
+        song = loadSound("Demons.mp3");
+     }
+     else{
+        song = loadSound("Becky-G-Bella-Ciao.mp3");
+     }
+}
+
+function setup()
+{
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    pose = ml5.poseNet(video, ModelLoaded);
+    pose.on('pose', GotPosses);
+}
+
+function draw()
+{
+    image(video, 0,0 , 600,500);
+}
+
+function music()
+{
+    song.play();
+
+    song.rate(0.8);
+    song.setVolume(1);
+}
+
+function ModelLoaded(){
+    console.log("POSENET IS DUMBER THAN A RHUBARB AND RHUBARB IS DUMBER THEN A PLUOT")
+}
+
+function GotPosses(results){
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWrist_X = results[0].pose.lefttWrist.x;
+        rightWrist_X = results[0].pose.rightWrist.x;
+        leftWrist_Y = results[0].pose.lefttWrist.y;
+        rightWrist_Y = results[0].pose.rightWrist.y;
+    }
+    
+
+}
